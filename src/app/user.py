ffa2bc7cde6f727af6f9f00add8fec021338954f
marie0501 <mariedelvallereyes01@gmail.com>
2023-07-12 06:23:23 -0400

    udpate user

--- src/app/user.py
+++ src/app/user.py
@@ -1,49 +1,20 @@
-import dictdatabase as DDB
 from app.domain.user import User
-import hashlib
 
+from kademlia.network import Server
 
-def digest(string):
-    if not isinstance(string, bytes):
-        string = str(string).encode('utf8')
-    return hashlib.sha1(string).hexdigest()
-
-
-file_name = 'data'
-
-def database():
-    DDB.config.storage_directory = "../database"
-    database = DDB.at(f"{file_name}")
-    if not database.exists():
-        database.create({})
-
-def get_user(alias):
-
-    database()  
-    key = digest(alias)   
-
-    if DDB.at(f"{file_name}", key=f"{key}").exists():
-        data = DDB.at(f"{file_name}", key=f"{key}").read()
+async def get_user(alias, server: Server):
+    data = await server.get(alias)
+    
+    if data == None:
+        return None
+    elif isinstance(data, dict):    
         user = User(data['alias'],data['full_name'], data['password'])
         user.active = data['logged']
         user.requests = data['inbox']
         user.workspaces = data['workspaces']
-
         return user
-        
-    
-    return None
+    else:
+        return data
 
-
-def set_user(alias, dicc):
-     
-    database()
-
-    key = digest(alias)
-    value = dicc    
-
-    with DDB.at(f"{file_name}").session() as (session, file):
-        file[f"{key}"] = value
-        session.write()
-
-
+def set_user(alias, dicc, server: Server):
+    server.set(alias, dicc)
--- src/app/workspace.py
+++ src/app/workspace.py
@@ -1,41 +1,33 @@
-from app.domain.user import User
-from app.domain.event import Event
 from app.domain.workspace import FlatWorkspace, HierarchicalWorkspace
-from app.domain.request import Request
-import dictdatabase as DDB
-import hashlib
-
 from kademlia.network import Server
-
-def digest(string):
-    if not isinstance(string, bytes):
-        string = str(string).encode('utf8')
-    return hashlib.sha1(string).hexdigest()
 
 
 async def get_workspace(workspace_id, server: Server):
-    key = digest(workspace_id)
-    data = await server.get(key)
+    data = await server.get(workspace_id)
     
-    workspace = None
-    if data['type'] == 'flat':
-        workspace = FlatWorkspace(data['name'],data['id'])
-        workspace.events = data['events']
-        workspace.users = data['users']
-        workspace.requests = data['requests']
-        workspace.waiting_events = data['waiting_events']
-        workspace.waiting_users = data['waiting_users']
+    if data == None:
+        return None
+    elif isinstance(data, dict):    
+        workspace = None
+        if data['type'] == 'flat':
+            workspace = FlatWorkspace(data['name'],data['id'])
+            workspace.events = data['events']
+            workspace.users = data['users']
+            workspace.requests = data['requests']
+            workspace.waiting_events = data['waiting_events']
+            workspace.waiting_users = data['waiting_users']
+        else:
+            workspace = HierarchicalWorkspace(data['name'],data['id'])
+            workspace.events = data['events']
+            workspace.users = data['users']
+            workspace.requests = data['requests']
+            workspace.waiting_users = data['waiting_users']
+            workspace.admins = data['admins']
+        
+        return workspace
     else:
-        workspace = HierarchicalWorkspace(data['name'],data['id'])
-        workspace.events = data['events']
-        workspace.users = data['users']
-        workspace.requests = data['requests']
-        workspace.waiting_users = data['waiting_users']
-        workspace.admins = data['admins']
-    
-    return workspace
+        return data
 
 
 def set_workspace(workspace_id, dicc, server: Server):
-    key = digest(workspace_id)
-    server.set(key, dicc)
+    server.set(workspace_id, dicc)
